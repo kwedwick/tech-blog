1a1900d592567f2c4d56780855d1423b1d3ed63a
KWedwick <64446581+kwedwick@users.noreply.github.com>
2020-08-28 18:44:41 -0500

    files and folders all set up

--- server.js
+++ server.js
@@ -1,19 +1,14 @@
-const express = require('express');
-const routes = require('./controllers');
-const sequelize = require('./config/connection');
 const path = require('path');
-const exphbs = require('express-handlebars');
+const express = require('express');
 const session = require('express-session');
-const helpers = require('./utils/helpers');
-const hbs = exphbs.create({helpers});
-
+const exphbs = require('express-handlebars');
 require('dotenv').config();
-
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
+const sequelize = require("./config/connection");
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
   secret: process.env.SESS_SECRET,
@@ -26,18 +21,20 @@ const sess = {
 };
 
 app.use(session(sess));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+
+const helpers = require('./utils/helpers');
+
+const hbs = exphbs.create({ helpers });
+
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
-// turn on routes
-app.use(routes);
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(require('./controllers/'));
 
-//force: makes Sequelize drop and recreate the tables
-// turn on connection to database and server
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
